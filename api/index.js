bd1da244d728efd5cec1522ac2db8b67b861431b
MildlyBrain <maethaphan.phai@gmail.com>
2025-03-25 18:33:13 +0700

    style: re-prettier again

--- api/index.js
+++ api/index.js
@@ -34,7 +34,7 @@ app.use("/api/auth", auth);
 const PORT = process.env.PORT || 5000;
 const server = app.listen(
   PORT,
-  console.log("Server running in", process.env.NODE_ENV, "mode on port", PORT)
+  console.log("Server running in", process.env.NODE_ENV, "mode on port", PORT),
 );
 
 process.on("unhandledRejection", (err, promise) => {
